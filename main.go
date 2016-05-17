49a635c6b56193476a23a3cd30958a8bd9aaea68
Vítězslav Ferko <vitezslavferko@gmail.com>
2016-05-17 16:16:58 +0200

    functions

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import "fmt"
 
 func main() {
 
-  // a := 6
-  // var b int = 10
-
-  // c := a + b
-
   slice := []int{1, 2, 3, 6, 12, 31, 45}
 
-  for i := 0; i < len(slice); i++ {
-    fmt.Println(slice[i])
-  }
+  fmt.Println(slice)
 
-  fmt.Println(c)
+  for index, value := range slice {
+    fmt.Println("slice[%d] = %d", index, value)
+  };
+
+  printSlice(slice)
+}
+
+func printSlice(slice []int) {
+  for _, value := range slice {
+    fmt.Printf("%dn", value)
+  }
 }
