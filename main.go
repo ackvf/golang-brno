77fcbea017a76d01b5d92c0f27f44676c3dae0b3
Vítězslav Ferko <vitezslavferko@gmail.com>
2016-05-17 16:52:15 +0200

    http mux

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import "net/http"
 
 func main() {
 
-  http.HandleFunc("/", rootHandler)
-  http.ListenAndServe(":8080", nil)
+  mux := http.NewServeMux()
 
+  mux.HandleFunc("/", rootHandler)
+  mux.HandleFunc("/todo", todoHandler)
+
+
+  http.ListenAndServe(":8080", mux)
 }
 
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Hello server2"))
+  w.Write([]byte("Hello server3"))
+}
+
+func todoHandler(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello todo"))
 }
