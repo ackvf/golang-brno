6658368fae527262c53ccf91e0e2b98414e3d364
Vítězslav Ferko <vitezslavferko@gmail.com>
2016-05-17 16:08:21 +0200

    3

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import "fmt"
 
 func main() {
 
-  a := 5
+  a := 6
   var b int = 10
 
   c := a + b
 
-
+  if c == 15 {
+    fmt.Println("yes it's 15")
+  } else {
+    fmt.Println("no it's not")
+  }
 
   fmt.Println(c)
 }
